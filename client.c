04e546fe911e7e63ef6380a2985479bed553a2c6
gszp1 <grzybekmateusz@poczta.fm>
2024-03-13 00:59:17 +0100

    Adds argument checking.

--- client.c
+++ client.c
@@ -2,4 +2,8 @@
 
 int main(int argc, char* argv[]) {
     //Check for arguments
+    if (argc < 3) {
+        printf("Not enough parameters provided.\n Required: IP address, port number.\n");
+        return 1;
+    }
 }
