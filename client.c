c7fc35b977de84f8310daa0f4fc664dfb48dfbfa
gszp1 <grzybekmateusz@poczta.fm>
2024-03-13 00:30:36 +0100

    Adds query_identificator_t struct.

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
+#include "uitls.h"
 
 int main(int argc, char* argv[]) {
-
+    //Check for arguments
 }
--- uitls.h
+++ uitls.h
@@ -1,7 +1,14 @@
 #ifndef UTILS.H
 #define UTILS.H
 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdint.h>
+
+typedef struct {
+    uint8_t query_type[4];
+    uint32_t request_id;
+} query_identificator_t;
 
 
-
-#endif //UTILS.H
+#endif //uitls.h
