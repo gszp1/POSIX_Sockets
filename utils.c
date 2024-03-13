59dc6df5813604d6eca5b05354dde7b7de50979b
gszp1 <grzybekmateusz@poczta.fm>
2024-03-13 10:01:13 +0100

    Adds functions definitions.

--- utils.c
+++ utils.c
@@ -1,5 +1,15 @@
 #include "utils.h"
 
+uint8_t check_endianess() {
+    // 0 if big endian, 1 if little endian
+    int i = 1;
+    if (*((char*) & i) == 1) {
+        return 1;
+    }
+    return 0;
+}
+
+
 uint32_t get_uint_bigendian(uint32_t val) {
     uint8_t res[4];
     int8_t i = 3;
@@ -20,6 +30,14 @@ uint64_t get_double_bigendian(double val) {
     return *((uint64_t*)res);
 }
 
+uint32_t get_uint_little_endian(uint32_t val) {
+
+}
+
+double get_double_little_endian(uint64_t val) {
+
+}
+
 // porty na bigendian
-// zrobić plik config 
+// zrobić plik config
 // sprawdzać czy jest big czy litlle endian przy zamianie na little
