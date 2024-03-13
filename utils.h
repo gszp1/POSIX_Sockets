2164a78b070cb7d4f5b30708c9f713c8c421e377
gszp1 <grzybekmateusz@poczta.fm>
2024-03-13 09:43:14 +0100

    Adds sections to header file.

--- utils.h
+++ utils.h
@@ -1,17 +1,24 @@
 #ifndef UTILS.H
 #define UTILS.H
 
+//// Includes ////
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdint.h>
 
+//// macros definitions ////
+
 #define BITS_PER_BYTE 8
+
+//// types definitions ////
 
 typedef struct {
     uint8_t query_type[4];
     uint32_t request_id;
 } query_identifier_t;
 
+//// functions declarations ////
 
 uint32_t get_uint_bigendian(uint32_t val);
 
