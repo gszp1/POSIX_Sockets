cab81353d9db2f48189abda7bc8947376d27f6ec
gszp1 <grzybekmateusz@poczta.fm>
2024-03-13 09:54:59 +0100

    Adds check_endianess and get_little_endian functions declarations.

--- utils.h
+++ utils.h
@@ -20,8 +20,14 @@ typedef struct {
 
 //// functions declarations ////
 
+uint8_t check_endianess()
+
 uint32_t get_uint_bigendian(uint32_t val);
 
 uint64_t get_double_bigendian(double val);
+
+uint32_t get_uint_little_endian(uint32_t val);
+
+double get_double_little_endian(uint64_t val);
 
 #endif //uitls.h
