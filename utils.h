0b19437f6b72960dfa9c4139b85b605ded3e67e5
gszp1 <grzybekmateusz@poczta.fm>
2024-03-20 02:10:41 +0100

    Utils.h refactor.

--- utils.h
+++ utils.h
@@ -1,7 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-//// headers includes////
+//// Included header files ////
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -16,52 +16,51 @@
 #include <time.h>
 #include <string.h>
 
-//// macros definitions ////
+//// Macro definitions ////
 
-#define BITS_PER_BYTE 8
 #define PROTOCOL_AUTO 0
 #define SQRT_MESSAGE 1
 #define DATE_MESSAGE 2
 #define REQUEST 0
 #define RESPONSE 1
 
-//// types definitions ////
+//// Type definitions ////
 
 typedef struct {
-    uint8_t query_type[4]; // request[0]/response[1] | 0 | 0 | sqrt[1]/date[2]
+    uint8_t query_type[4]; // REQUEST/RESPONSE | 0 | 0 | SQRT_MESSAGE/DATE_MESSAGE
     uint32_t message_id;
 } query_header_t;
 
-//// functions declarations ////
+//// Function declarations ////
 
-// checking machine endianess, 0 - big endian, 1 - little endian
-uint8_t check_endianess();
-
-// converts little endian double to big endian uint64_t
-uint64_t get_double_bigendian(double val);
-
-// converts big endian uint64_t to little endian double
-double get_double_little_endian(uint64_t val);
-
-// writes data to sockfd socket safely (re-writes when it is necessary)
-int8_t safe_write(void* data, size_t length, int sockfd);
-
-// reads data from sockfd socket safely (re-reads when it is necessary)
-int8_t safe_read(void* data, size_t length, int sockfd);
-
-// sends message to socket
-int8_t send_message(query_header_t* header, void* msg_data, size_t data_length, int sockfd);
-
-// checks if header is correctly set
-int8_t validate_header(query_header_t* header);
-
-// reads message from socket
-int8_t read_message(query_header_t* header, void** msg, size_t* data_length, int sockfd);
-
-// wrapper for parsing host double to network double
+// Wrapper for parsing host double to network double
 uint64_t htond(double host_double);
 
-// wrapper for parsing network double to host double
+// Wrapper for parsing network double to host double
 double ntohd(uint64_t net_double);
+
+// Converts little endian double to big endian uint64_t
+uint64_t get_double_bigendian(double val);
+
+// Converts big endian uint64_t to little endian double
+double get_double_little_endian(uint64_t val);
+
+// Checks machine endianess, 0 - big endian, 1 - little endian
+uint8_t check_endianess();
+
+// Sends message to socket
+int8_t send_message(query_header_t* header, void* msg_data, size_t data_length, int sockfd);
+
+// Writes data to sockfd socket safely (re-writes when not all bytes were sent)
+int8_t safe_write(void* data, size_t length, int sockfd);
+
+// Reads message from socket
+int8_t read_message(query_header_t* header, void** msg, size_t* data_length, int sockfd);
+
+// Reads data from sockfd socket safely (re-reads when not all bytes were read)
+int8_t safe_read(void* data, size_t length, int sockfd);
+
+// Checks if header is correctly set
+int8_t validate_header(query_header_t* header);
 
 #endif //UTILS_h
